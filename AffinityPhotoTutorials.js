c92e4cebe30172d2fd4ee5d5c261ef6b6a9e2830
k38 <matumoto.stc@gmail.com>
2019-10-16 01:19:57 +0900

    Add: AffinityPhotoTutorials.js

---
+++ AffinityPhotoTutorials.js
@@ -0,0 +1,9 @@
+javascript: (() => {
+    window.list = () => {
+        const h2 = document.querySelectorAll(".c-tutorial-item h2");
+        const items = [];
+        for( i=0; i<h2.length; i++ ){ items.push(h2[i].textContent.trim()) }
+        return items.join("\n");
+    };
+    console.log("call 'list()'");
+})()
